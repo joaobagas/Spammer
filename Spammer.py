2986cfb361a14ce541e620398fe315506b372530
joaobagas <joaobagas@users.noreply.github.com>
2020-05-09 15:41:25 +0200

    Update Spammer.py

--- Spammer.py
+++ Spammer.py
@@ -2,3 +2,17 @@ import requests
 import os
 import json
 import random
+
+url = 'https://curiouscat.qa/api/v2/post/create'
+
+questions = json.loads(open('StupidQuestions.json').read)
+
+for quetion in questions:
+    request.post(url, allow_redirects=False, data = {
+        'to': '' #Insert username here
+        'anon': 'true'
+        'question': question
+        'in_response_to': 'undefined'
+        '_ob': 'registerOrSignin'
+    })
+    
