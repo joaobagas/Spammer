ab1082cbafade7cfd7405f991ab9e45f747d9ecd
joaobagas <joaobagas@users.noreply.github.com>
2020-05-10 10:41:38 +0200

    Now the script send random messages and you can choose how many of them you want to send

--- Spammer.py
+++ Spammer.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import json
+import random
 
 url = 'https://curiouscat.qa/api/v2/post/create'
 
@@ -8,10 +9,12 @@ questions = json.loads(open('StupidQuestions.json').read())
 print("Json file loaded...")
 
 username = input("Insert the username you want to spam here >> ")
+nMessages = int(input("How many messages do you want to send >> "))
 
 print("Spam started...")
 
-for question in questions:
+for i in range(nMessages):
+    question = questions[random.randint(0,91)]
     requests.post(url, allow_redirects=False, data = {
         'to': username,
         'anon': 'true',
@@ -19,6 +22,7 @@ for question in questions:
         'in_response_to': 'undefined',
         '_ob': 'registerOrSignin'
     })
+    print(question)
     
 print("Spam finished...")
     
